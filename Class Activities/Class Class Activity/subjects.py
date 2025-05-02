0b4a2c1fe6d5da459901448d69074098ae60d455
RonenGupta <160193990+RonenGupta@users.noreply.github.com>
2025-05-02 10:10:17 +1000

    Did CLASSES

---
+++ Class Activities/Class Class Activity/subjects.py
@@ -0,0 +1,2 @@
+class Subject:
+    def __init__(self, 
---
+++ Class Activities/Game Objects/main.py
@@ -0,0 +1,43 @@
+from game_objects import Player, Weapon, Enemy # MUST have this in the program
+import random #For random damage and health
+characters = []
+# Create an instance of Player
+player_character1 = Player('Gimli', 'Dwarf', 'Fighter', 3, 180)
+characters.append(player_character1)
+
+player_character2 = Player('Victor', 'Guo', 'Computer Nerd', 1, 5432780537205732985743205235732805743820574380275483025743025749023578432057438025743820532704532050)
+characters.append(player_character2)
+
+player_character3 = Player('Levin', 'Shao', 'Egg', 0, 29137239139321723857129856478218943287472972653428064738025784923563295462354792356492356423785623795423795437564789235647892356425439)
+characters.append(player_character3)
+
+player_character4 = Player('Ronen', 'Gupta', 'idk', 3413431483194813948104, 1804314713947147310497318947139)
+characters.append(player_character4)
+
+
+
+
+
+# TODO: Create an instance of Weapon with random damage between 12 and 15
+weapon_sword = Weapon('Dragonslayer', 'Melee', random.randint(12, 15))
+# TODO: Create an instance of Enemy with random damage between 15 and 18, and random health between 80 and 140
+enemy_character = Enemy('Mr Scott', 'Programmer', random.randint(15,18), random.randint(80, 140))
+
+# Print the player character details
+print(f"Player Name: {player_character.name}")
+print(f"Player Race: {player_character.race}")
+print(f"Player Class: {player_character.cls}")
+print(f"Player Attack: {player_character.atk}")
+print(f"Player Health: {player_character.health}")
+
+# TODO: Print the player weapon details
+print(f"Weapon Name: {weapon_sword.wpnname}")
+print(f"Weapon Category: {weapon_sword.category}")
+print(f"Weapon Damage: {weapon_sword.damage}")
+
+# TODO: Print the enemy character details
+print(f"Enemy Name: {enemy_character.enmname}")
+print(f"Enemy Race: {enemy_character.enmrace}")
+print(f"Enemy Damage: {enemy_character.enmdamage}")
+print(f"Enemy Health: {enemy_character.enmhealth}")
+
